effece93c881d9abd0c0bd836d1404aec93aa462
acenine <acenine@gmx.com>
2017-11-08 23:14:17 -0800

    Pass all tests for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,28 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+  	return 'null';
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+  	return obj.toString();
+  }
+  if (typeof obj === 'function' || obj === undefined || typeof obj === 'symbol') {
+  	return undefined;
+  }
+  if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+  	return "[" + obj.map(stringifyJSON).join(',') + "]";
+  }
+  else {
+  	let result = [];
+  	for (let item in obj) {
+  	  if (stringifyJSON(obj[item]) !== undefined){
+  	  	result.push(stringifyJSON(item) + ":" + stringifyJSON(obj[item]));
+  	  }
+  	}
+  	return "{" + result.join(",") + "}";
+  }
 };
